5902e44a89fd9fa145210b115a124f9117a6c1b5
28nam <vga29-ac220087@sankogakuen.jp>
2023-11-22 14:45:57 +0900

    22/11/2023 編集
    22/11/2023

--- Assets/Scripts/RollABall/InGame/PendulumBlock.cs
+++ Assets/Scripts/RollABall/InGame/PendulumBlock.cs
@@ -8,13 +8,24 @@ public class PendulumBlock : MonoBehaviour
     public float Amplitude = 5f;//êUïù
     public float Frequency = 1f; //ïpìx
     private Vector3 startPos= Vector3.zero;
+
+    private MeshRenderer boxMesh;
+    float time = 1f;
     private void Start() { 
         startPos = transform.position; 
+        boxMesh = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = startPos + Vector3.forward * Amplitude * Mathf.Sin(Time.time * Frequency);
+
+        time -= Time.deltaTime;
+        if (time < 0f)
+        {
+            boxMesh.enabled = !boxMesh.enabled;
+            time = 1f;
+         }
     }
 }
